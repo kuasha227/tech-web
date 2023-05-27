9ce59a1bb544addfdd15bd8d49855cec8f576b91
kuasha227 <kuasha227@gmail.com>
2023-05-27 21:42:35 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
+import Header from "./Components/Header";
 
 
 function App() {
   return (
-    <div className="App">
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <Header />
     </div>
   );
 }
