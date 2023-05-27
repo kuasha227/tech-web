7b6724a0f3e5e225d36417b5d6a15e04b2165f0f
kuasha227 <kuasha227@gmail.com>
2023-05-28 02:55:54 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import Banner from "./Components/Banner";
+import Experts from "./Components/Experts";
 import Header from "./Components/Header";
+import Subscription from "./Components/Subscription";
 
 
 function App() {
   return (
     <div>
       <Header />
+      <Banner />
+      <Experts />
+      <Subscription />
     </div>
   );
 }
